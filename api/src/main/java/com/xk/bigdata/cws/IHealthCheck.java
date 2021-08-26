0a9cd48b98ff5a076f00562e781ff2d4529148d5
xiankai <xiankai@hikvision.com.cn>
2021-08-26 10:19:32 +0800

    [update]vue集成

--- api/src/main/java/com/xk/bigdata/cws/IHealthCheck.java
+++ api/src/main/java/com/xk/bigdata/cws/IHealthCheck.java
@@ -1,17 +1,17 @@
 package com.xk.bigdata.cws;
 
-import org.springframework.stereotype.Controller;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.net.HttpRetryException;
 import java.util.Map;
 
 @Path("system")
 public interface IHealthCheck {
 
     @Path("/health")
-    @POST
+    @GET
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces("application/json")
-    Map<String, Object> healthCheck(Map<String,Object> data);
+    Map<String, Object> healthCheck();
 }
--- modules/provider/src/main/java/com/xk/bigdata/cws/HealthCheckController.java
+++ modules/provider/src/main/java/com/xk/bigdata/cws/HealthCheckController.java
@@ -1,5 +1,7 @@
 package com.xk.bigdata.cws;
 
+import org.apache.http.HttpRequest;
+import org.apache.log4j.spi.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -12,8 +14,10 @@ import java.util.Map;
 @Component
 public class HealthCheckController implements IHealthCheck {
 
+
     @Override
-    public Map<String, Object> healthCheck(Map<String,Object> data) {
+    public Map<String, Object> healthCheck() {
+        System.out.println("我收到了一个请求");
         Map<String,Object> map = new HashMap<>();
         map.put("status","running");
         return map;
